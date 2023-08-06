bd5fc524b2e3119f6424155c2d6930f94baa8630
JasonStaubach <jbstaubach@gmail.com>
2023-08-06 09:27:29 -0400

    added first part of csurf

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors');
+const csurf = require('csurf');
+
+const { isProduction } = require('./config/keys');
 
 const usersRouter = require('./routes/api/users');
 const tweetsRouter = require('./routes/api/tweets');
@@ -11,6 +15,13 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+if (!isProduction) {
+    // Enable CORS only in development because React will be on the React
+    // development server (http://localhost:3000). (In production, the Express 
+    // server will serve the React files statically.)
+    app.use(cors());
+  }
 
 app.use('/api/users', usersRouter);
 app.use('/api/tweets', tweetsRouter);
